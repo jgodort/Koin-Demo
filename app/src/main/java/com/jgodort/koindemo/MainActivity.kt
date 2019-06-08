ad3d883df0e52f42687283c71527c36ec75f0ee2
Javier Godino <jgodort.software@gmail.com>
2019-06-09 00:53:46 +0200

    created recycler view

--- app/src/main/java/com/jgodort/koindemo/MainActivity.kt
+++ app/src/main/java/com/jgodort/koindemo/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.jgodort.koindemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.recycler_currencies
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  val currenciesAdapter: CurrenciesAdapter by inject()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    setupCurrenciesRecycler()
+  }
+
+  private fun setupCurrenciesRecycler() {
+    recycler_currencies.layoutManager = LinearLayoutManager(this)
+    recycler_currencies.adapter=currenciesAdapter
+  }
 }
