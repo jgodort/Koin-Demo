129ed9d26a110b36e8530df2d5a3ea9ef11811ae
Javier Godino <jgodort.software@gmail.com>
2019-06-16 18:07:24 +0200

    Injection of dependecies on main activity

--- app/src/main/java/com/jgodort/koindemo/MainActivity.kt
+++ app/src/main/java/com/jgodort/koindemo/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.jgodort.koindemo
 
+import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.recycler_currencies
+import com.jgodort.koindemo.presentation.CurrenciesAdapter
+import com.jgodort.koindemo.presentation.CurrenciesViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-  val currenciesAdapter: CurrenciesAdapter by inject()
+    val currenciesAdapter: CurrenciesAdapter by inject()
+    val currenciesViewModel: CurrenciesViewModel by viewModel()
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
 
-    setupCurrenciesRecycler()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupCurrenciesRecycler()
 
-  private fun setupCurrenciesRecycler() {
-    recycler_currencies.layoutManager = LinearLayoutManager(this)
-    recycler_currencies.adapter=currenciesAdapter
-  }
+        currenciesViewModel.currenciesLiveData.observe(this, Observer {
+            currenciesAdapter.currencies=it!!
+        })
+        val currenciesJson = resources.openRawResource(R.raw.currencies)
+            .bufferedReader().use { it.readText() }
+        currenciesViewModel.retrieveCurrencies(currenciesJson)
+    }
+
+    private fun setupCurrenciesRecycler() {
+        recycler_currencies.layoutManager = LinearLayoutManager(this)
+        recycler_currencies.adapter = currenciesAdapter
+    }
 }
--- app/src/main/java/com/jgodort/koindemo/modules.kt
+++ app/src/main/java/com/jgodort/koindemo/modules.kt
@@ -1,9 +1,24 @@
 package com.jgodort.koindemo
 
 import com.google.gson.Gson
+import com.jgodort.koindemo.data.DataRepository
+import com.jgodort.koindemo.data.DataRepositoryFactory
+import com.jgodort.koindemo.data.LocalDataRepositoryImpl
+import com.jgodort.koindemo.data.RemoteDataRepositoryImpl
+import com.jgodort.koindemo.presentation.CurrenciesAdapter
+import com.jgodort.koindemo.presentation.CurrenciesViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val applicationModule = module {
-  single { Gson() }
-  factory { CurrenciesAdapter() }
+    single { Gson() }
+    single { UrlHelper() }
+
+    factory { CurrenciesAdapter() }
+    factory<DataRepository>(named("Local")) { LocalDataRepositoryImpl(get()) }
+    factory<DataRepository>(named("Remote")) { RemoteDataRepositoryImpl(get()) }
+    factory { DataRepositoryFactory(get(named("Local")), get(named("Remote"))) }
+
+    viewModel { CurrenciesViewModel(get()) }
 }
